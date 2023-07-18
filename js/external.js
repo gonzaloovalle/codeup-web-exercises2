774fb99a95740a0be5ca7c9a6a9038ebfd1a4bd6
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2023-07-18 12:28:58 -0500

    js with html exercise pt2

--- js/external.js
+++ js/external.js
@@ -30,3 +30,42 @@ alert(`Little Mermaid will cost: $${littleMermaidPrice}, Brother Bear will cost:
 
 
 
+let google = prompt("How much does Google pay?");
+
+let amazon = prompt("How much does Amazon pay?");
+
+let facebook = prompt("How much does Facebook pay?");
+
+let googleHours = prompt("How many hours did you work for Google?");
+
+let amazonHours = prompt("How many hours did you work for Amazon?");
+
+let facebookHours = prompt("How many hours did you work for Facebook?");
+
+let googlePay = google * googleHours;
+
+let amazonPay = amazon * amazonHours;
+
+let facebookPay = facebook * facebookHours;
+
+alert(`You will earn: $${googlePay} from Google, $${amazonPay} from Amazon, and $${facebookPay} from Facebook. Your total pay will be: $${googlePay + amazonPay + facebookPay}`);
+
+
+
+let classNotFull = confirm("The class is not full");
+
+let noConflicts = confirm("There are no schedule conflicts");
+
+let studentEnrolled = classNotFull && noConflicts;
+
+alert(`Student enrolled: ${studentEnrolled}`);
+
+
+
+let itemsBought = prompt("How many items have you bought?");
+
+let premiumMember = confirm("Are you a premium member?");
+
+let discountCheck = (itemsBought > 2) && premiumMember;
+
+alert(`Eligible for discount: ${discountCheck}`);
