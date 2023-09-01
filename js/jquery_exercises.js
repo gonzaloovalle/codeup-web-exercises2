d030fdd6f56cf9b33d9a89a7b254df6cc2cd1f7c
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2023-09-01 14:42:00 -0500

    finished jquery selectors exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,3 +1,19 @@
-window.onload = () => {
-    alert("The page has finished loading!")
-}
+//jQuery Intro exercise
+// window.onload = () => {
+//     alert("The page has finished loading!")
+// }
+
+//jQuery Selectors exercise
+// let headingContent = $("#greeting").html();
+// let paragraphContent = $("#lorem").html();
+//
+// alert(headingContent);
+// alert(paragraphContent);
+
+// $(".codeup").css("border", "1px solid red")
+
+// $("li").css("font-size", "20px")
+//
+// $("h1, p, li").css("background-color", "yellow");
+//
+// alert($("h1").html());
