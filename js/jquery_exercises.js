fc6e6f6502c702065126b736878a529d39fb3b6b
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2023-09-05 09:11:26 -0500

    completed jquery mouse events exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -17,3 +17,29 @@
 // $("h1, p, li").css("background-color", "yellow");
 //
 // alert($("h1").html());
+
+//jQuery Mouse Events exercise
+$("h1").click(() => {
+    $("h1").css("background-color", "red");
+});
+
+$("p").dblclick(() => {
+    $("p").css("font-size", "18px");
+});
+
+// $("li").hover(
+//     () => {
+//         $("li").css("color", "red");
+//     },
+// () => {
+//         $("li").css("color", "black");
+//     }
+// );
+
+$("li").mouseenter(() => {
+    $("li").css("color", "red");
+});
+
+$("li").on("mouseleave",() => {
+    $("li").css("color", "black");
+});
